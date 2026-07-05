a8546ecacde4496ff86fd88bca58724ca4eec1f6
Rahul-Singh <rahulsinghraj2000@gmail.com>
2026-07-05 22:52:15 +0530

    statement for Cursor

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import Cursor from './components/Cursor'
+import Cursor from './components/Cursor.jsx'
 
 
 
